65fcb7e174f6db6f6a8c9dc976df919e0368cfac
simbahebinbo <lansheng228@protonmail.com>
2021-11-16 10:57:06 +0800

    升级

--- src/main/java/com/supernova/mysqlmockdemo/MysqlMockdbDemoApplication.java
+++ src/main/java/com/supernova/mysqlmockdemo/MysqlMockdbDemoApplication.java
@@ -2,12 +2,10 @@ package com.supernova.mysqlmockdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.supernova.mysqlmockdemo")
-@EnableAutoConfiguration
 @EnableConfigurationProperties
 @EnableJpaRepositories(basePackages = "com.supernova.mysqlmockdemo")
 public class MysqlMockdbDemoApplication {
